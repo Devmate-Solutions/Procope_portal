be97059ff2a5ee06b18701e619931ef04d113789
Fazal wahab <114792467+Fazalwahab12@users.noreply.github.com>
2025-08-23 02:13:35 +0500

    Update page.tsx

--- src/app/create-calls/page.tsx
+++ src/app/create-calls/page.tsx
@@ -2600,16 +2600,7 @@ Ayaz,Momin,20/3/1983,19293900101,gave anesthesia for surgery,was told to not eat
                 </div>
               </div>
 
-              {/* Divider */}
-              <div className="relative">
-                <div className="absolute inset-0 flex items-center">
-                  <span className="w-full border-t" />
-                </div>
-                <div className="relative flex justify-center text-xs uppercase">
-                  <span className="bg-background px-2 text-muted-foreground">Or paste CSV data</span>
-                </div>
-              </div>
-
+             
            
           
 
